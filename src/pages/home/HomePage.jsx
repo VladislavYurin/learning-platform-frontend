bd6e08daa55fdf2ae5972535b50b164a87935a85
Vladislav Yurin <vdiurin@spasibosb.ru>
2025-12-25 17:00:23 +0300

    1

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { Button, Container, Typography, Grid, Card, CardContent } from '@mui/material';
+import {Card, CardContent, Container, Grid, Typography} from '@mui/material';
 
 const HomePage = () => {
-    const { isAuthenticated } = useSelector(state => state.auth);
-
     return (
         <Container maxWidth="lg">
             <Typography variant="h2" gutterBottom align="center" mt={4}>
-                Добро пожаловать на образовательную платформу
+                Добро пожаловать на образовательную платформу. Убейте меня !!!!!
             </Typography>
 
             <Typography variant="h5" gutterBottom align="center" mb={4}>
                 Изучайте новые технологии с лучшими менторами
             </Typography>
-
-            {!isAuthenticated && (
-                <Grid container spacing={2} justifyContent="center" mb={4}>
-                    <Grid item>
-                        <Button variant="contained" color="primary" component={Link} to="/login">
-                            Войти
-                        </Button>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="outlined" color="primary" component={Link} to="/register">
-                            Зарегистрироваться
-                        </Button>
-                    </Grid>
-                </Grid>
-            )}
 
             <Grid container spacing={4}>
                 <Grid item xs={12} md={4}>
@@ -38,16 +19,6 @@ const HomePage = () => {
                             <Typography variant="h5" gutterBottom>Курсы</Typography>
                             <Typography>
                                 Доступные курсы по различным технологиям и направлениям разработки
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography variant="h5" gutterBottom>Модули</Typography>
-                            <Typography>
-                                Структурированные модули с теорией и практическими заданиями
                             </Typography>
                         </CardContent>
                     </Card>
